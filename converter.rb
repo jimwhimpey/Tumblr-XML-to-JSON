07306afb0601c6061d92d7087c923da8806e08e4
jimwhimpey <jimwhimpey@me.com>
2010-11-19 20:56:52 +1000

    Started writing a heap of manual logic before thinking, wait a minute, there's probably some ruby thing that straight up converts XML to JSON. Trying that now.

--- converter.rb
+++ converter.rb
@@ -1,7 +1,9 @@
 require 'rubygems'
 require 'sinatra'
 require 'curb'
+require 'json'
 require 'nokogiri'
+require 'hpricot'
 require 'htmlentities'
 
 configure :development do
@@ -12,6 +14,41 @@ end
 set :haml, {:format => :html5, :escape_html => false}
 
 get '/' do
-  # Render the HAML template
-  haml :home
+	
+	# Start up the JSON string
+	json = "{"
+	
+	# Get the XML and process it into a nokogiri doc
+	xml_call = Curl::Easy.perform("http://daydreamtheme.tumblr.com/")
+	doc = Hpricot(xml_call.body_str)
+		
+	# Start a looping
+	doc.search("//data/").each do |element|
+		
+		# Only get elements, not text or comments
+		if (element.is_a?(Hpricot::Elem))
+			
+			# If it's not a block element
+			if (!/^block./.match(element.name))
+				
+				# Add it to the JSON, escaping the quotes
+				json += '"' + element.name + '": "' + element.inner_html.gsub(/["]/, '\\\\"') + '",'
+				
+			else
+				
+				# It's a block so we'll dive deeper
+				
+				
+			end
+			
+		end
+		
+	end
+	
+	# Remove the final comma Close the JSON string
+	json.chop! << "}"
+	
+	content_type :json
+	json
+	
 end

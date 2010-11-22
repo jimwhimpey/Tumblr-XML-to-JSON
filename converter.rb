ee237ffe899394749f174303dca79987c858eb46
jimwhimpey <jimwhimpey@me.com>
2010-11-22 17:43:52 +1000

    Fixed up the var declaration

--- converter.rb
+++ converter.rb
@@ -52,7 +52,7 @@ get %r{/content/?([^\/]*)/?$} do
 	doc = Hpricot::XML(xml_call.body_str)
 	
 	# Call the recursive convertXML function
-	json = convertXML(doc.search("//data"))
+	json = "var rawTumblrData = " + convertXML(doc.search("//data")) + ";"
 	
 	content_type 'text/javascript', :charset => 'utf-8'
 	
@@ -72,7 +72,7 @@ end
 def convertXML(xml)
 	
 	# Start up the JSON string we'll eventually return
-	json = "var rawTumblrData = {"
+	json = "{"
 	
 	# Start the loop
 	xml.search("/").each do |element|
@@ -121,6 +121,6 @@ def convertXML(xml)
 	end
 	
 	# Remove the final comma Close the JSON string
-	return json.chop! << "};"
+	return json.chop! << "}"
 	
 end
